97a8d5b23885a61c7956a0f5afb83857c64051a8
lennox charles <lennyaiko17@gmail.com>
2023-10-19 17:50:10 +0100

    feat: use virtual db

--- main.py
+++ main.py
@@ -4,34 +4,15 @@ import json
 app = Flask(__name__)
 # Treblle(app)
 
+## DB
+articles_db = []
+users_db = []
+
 ## helpers
 
-def read_file(file_name):
-    f = open(f'data/{file_name}.json', 'r')
-    db = f.read()
-    db = db.replace('\'', '"')
-    db = json.loads(db)
-    f.close()
-    return db
-
-def save_file(db, file_name):
-    f = open(f'data/{file_name}.json', 'w')
-    f.write(str(json.dumps(db)))
-    f.close()
 
 ## callbacks
 
-@app.before_request
-def before_request_callback():
-    global articles_db, users_db
-    articles_db = read_file('articles')
-    users_db = read_file('users')
-
-@app.after_request
-def after_request_callback(response):
-    save_file(articles_db)
-    save_file(users_db)
-    return response
 
 ## routes
 
